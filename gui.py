531696e7dd16b1e85c1293a6339d6c3bfb1fb174
Ms Lawliet <alexieanoya@gmail.com>
2023-07-15 12:29:45 +0800

    created label and entry widgets for emergency contact first name

--- gui.py
+++ gui.py
@@ -15,13 +15,13 @@ root.title("Contact Tracing App")
 title = ctk.CTkLabel(root, text="CONTACT TRACING")
 title.place(x=175, y=10)
 
-# create label for each category
+# create category labels
 category1 = ctk.CTkLabel(root, text="Personal Information")
 category1.place(x=5, y=30)
 category2 = ctk.CTkLabel(root, text="Emergency Contact")
 category2.place(x=5, y=240)
 
-# create label for personal info
+# create labels for info
 first_name = ctk.CTkLabel(root, text="First Name")
 first_name.place(x=15, y=60)
 surname = ctk.CTkLabel(root, text="Surname")
@@ -32,8 +32,10 @@ phone = ctk.CTkLabel(root, text="Phone Number")
 phone.place(x=15, y=165)
 address = ctk.CTkLabel(root, text="Home Address")
 address.place(x=15, y=200)
+first_name2 = ctk.CTkLabel(root, text="First Name")
+first_name2.place(x=15, y=265)
 
-# create entry buttons for personal info
+# create entry buttons
 first_name_entry = ctk.CTkEntry(root)
 first_name_entry.place(x=85, y=60)
 surname_entry = ctk.CTkEntry(root)
@@ -44,5 +46,7 @@ phone_entry = ctk.CTkEntry(root, width=175)
 phone_entry.place(x=105, y=165)
 address_entry = ctk.CTkEntry(root, width=275)
 address_entry.place(x=105, y=200)
+first_name2_entry = ctk.CTkEntry(root)
+first_name2_entry.place(x=105, y=265)
 
 root.mainloop()
