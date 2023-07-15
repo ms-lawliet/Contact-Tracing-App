4612e60830394940847caa8d641eab8f283e3a31
Ms Lawliet <alexieanoya@gmail.com>
2023-07-15 11:18:57 +0800

    created method to add main window

--- gui.py
+++ gui.py
@@ -3,6 +3,10 @@ import tkinter as tk
 from info_class import ContactTracing
 
 # create method
+root = tk.Tk()
+root.geometry("475x560")
+root.title("Contact Tracing App")
+
 # create variables
 # create label
 # create entry buttons
