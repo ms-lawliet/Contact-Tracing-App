f59ff4bac81c3c72b171611f62801437727a389b
Ms Lawliet <alexieanoya@gmail.com>
2023-07-15 12:10:12 +0800

    modified contact tracing class

--- info_class.py
+++ info_class.py
@@ -1,21 +1,18 @@
 # create class for info
 class ContactTracing:
-    def personal_info(self, fn, sn, email, phone, st, cprov, zipc):
+    def personal_info(self, fn, sn, email, phone, address):
         first_name = (fn.get())
         surname = (sn.get())
         email_address = (email.get())
         phone_number = (phone.get())
-        street = (st.get())
-        city_or_province = (cprov.get())
-        zip_code = (zipc.get())
+        address = (address.get())
 
-    def emergency_contact(self, fn, sn, st, phone, cprov, zipc, rs):
+    def emergency_contact(self, fn, sn, st, phone, address, rs):
         first_name = (fn.get())
         surname = (sn.get())
         phone_number = (phone.get())
         street = (st.get())
-        city_or_province = (cprov.get())
-        zip_code = (zipc.get())
+        address = (address.get())
         relationship = (rs.get())
 
     def contacts(self, cfn, csn, cphone, cd):
