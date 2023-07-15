b16e8ae16d4c0d20dc2df3756c95dffd5078aea1
Ms Lawliet <alexieanoya@gmail.com>
2023-07-15 11:28:27 +0800

    imported another module for custom design and modified some lines

--- gui.py
+++ gui.py
@@ -1,12 +1,18 @@
 # import modules
 import tkinter as tk
+import customtkinter
 from info_class import ContactTracing
 
+# change theme to dark
+customtkinter.set_appearance_mode("dark")
+
 # create method
-root = tk.Tk()
+root = customtkinter.CTk()
 root.geometry("475x560")
 root.title("Contact Tracing App")
 
 # create variables
 # create label
 # create entry buttons
+
+root.mainloop()
