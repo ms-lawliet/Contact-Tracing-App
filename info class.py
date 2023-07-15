dca2a11fe88f83e593078d6b97e497e903575db4
Ms Lawliet <alexieanoya@gmail.com>
2023-07-15 11:04:54 +0800

    created function for getting personal_info

--- info class.py
+++ info class.py
@@ -1,1 +1,10 @@
 # create class for info
+class ContactTracing:
+    def personal_info(self, fn, sn, email, phone, st, cprov, zipc):
+        first_name = (fn.get())
+        surname = (sn.get())
+        email_address = (email.get())
+        phone_number = (phone.get())
+        street = (st.get())
+        city_or_province = (cprov.get())
+        zip_code = (zipc.get())
